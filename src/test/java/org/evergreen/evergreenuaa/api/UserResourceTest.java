cb1001f2d367eddad6044c450d845548594ac956
jackchen10 <jack-chen10@163.com>
2023-04-13 20:49:26 +0800

    新增MockMvc方式进行Mock单元测试

---
+++ src/test/java/org/evergreen/evergreenuaa/api/UserResourceTest.java
@@ -0,0 +1,38 @@
+package org.evergreen.evergreenuaa.api;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+public class UserResourceTest {
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @WithMockUser(username = "sec_user")
+    @Test
+    public void greeting1Test() throws Exception{
+        mockMvc.perform(
+                    get("/api/greeting1")
+                ).andExpect(status().isOk());
+    }
+}
